21ec336446ff533edc06ba5a09b231744b6dd9f0
Audun Jensvoll <audun@kongarthur.no>
2018-11-22 08:14:19 +0100

    Bruke "virkelig" rest post

--- js/afrlib.js
+++ js/afrlib.js
@@ -16,3 +16,16 @@ function get(url, onOk) {
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }        
+
+function post(url, data, onOk) {
+    var xmlhttp = new XMLHttpRequest();
+    
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            onOk(JSON.parse(this.responseText));
+        }
+    };    
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json");
+    xmlhttp.send(data);
+}        
